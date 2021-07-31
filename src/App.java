5f5e8a10e398f792a9cd82bad2e85bc690119d1e
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-07-31 06:27:19 -0300

    Estudos iniciais sobre Java

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int idade = 27; 
+        String nome = "Augusto"; 
+
+        if (idade <= 25){
+            System.out.println(nome + ", você está com " + idade + " anos!");
+        } else {
+            System.out.println("Você é um jovem, não é mesmo?");
+        }
+    }
+}
+
