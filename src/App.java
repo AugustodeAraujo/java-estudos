080267e29213ecf584cdb4f6f5e8ed51a7987623
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-07-31 06:50:54 -0300

    Estudos de While e For

--- src/App.java
+++ src/App.java
@@ -1,14 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int idade = 27; 
-        String nome = "Augusto"; 
+        int idade = 27;
+        String nome = "Augusto";
+        boolean humano = false;
 
-        if (idade <= 25){
-            System.out.println(nome + ", você está com " + idade + " anos!");
+        if (idade >= 25 && humano == true) {
+            System.out.println(nome + ", você está com " + idade + " anos é um humano!");
         } else {
-            System.out.println("Você é um jovem, não é mesmo?");
+            System.out.println("Você é um humano, "
+            + "não é mesmo?");
         }
+        
     }
 }
-
---
+++ src/testeWhile.java
@@ -0,0 +1,16 @@
+public class testeWhile {
+    public static void main(String[] args){
+
+        int contador = 0;
+
+        while(contador < 100){
+            contador++; 
+        }
+
+        System.out.println(contador);
+
+    }
+
+}
+
+
